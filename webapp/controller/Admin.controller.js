9fd9e3ba7a0360d7b7b152b366b85f9973456521
Julia Montiel González <juliamonlez07@gmail.com>
2026-05-26 09:01:42 +0000

    Demo simple registros

--- webapp/controller/Admin.controller.js
+++ webapp/controller/Admin.controller.js
@@ -1,23 +1,40 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/routing/History"
-], function (Controller, History) {
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("visitas.cero.visitas0.controller.Admin", {
 
         onInit: function () {
 
+            const oData = {
+                visitas: [
+                    {
+                        empresa: "Demo 1",
+                        empleado: "Juan",
+                        fecha: "2026-01-01",
+                        estado: "OK"
+                    },
+                    {
+                        empresa: "Demo 2",
+                        empleado: "Ana",
+                        fecha: "2026-01-02",
+                        estado: "OK"
+                    }
+                ]
+            };
+
+            const oModel = new JSONModel(oData);
+            this.getView().setModel(oModel, "demo");
         },
 
         onBack: function () {
-
             this.getOwnerComponent().getRouter().navTo("RouteVisitas0");
-
         },
 
         onDelete: function () {
-
+            console.log("delete");
         }
 
     });
